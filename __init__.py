37d9a1804fb479b654db541e8bbcebbe5488a364
George Sakkis <george.sakkis@gmail.com>
2014-11-08 23:55:20 +0200

    API and implementation changes + more tests

--- __init__.py
+++ __init__.py
@@ -6,34 +6,63 @@ import redis
 
 class RoundRobin(object):
 
-    redis_round_format = 'redrobin:{name}:round'
+    # sorted set of items sorte by availability time
+    redis_items_format = 'redrobin:items:{name}'
+    # hash of {item: throttle}
+    redis_throttles_format = 'redrobin:throttles:{name}'
 
-    def __init__(self, items=None, default_throttle=0, connection=None, name='default'):
+    def __init__(self, connection=None, default_throttle=0, name='default'):
+        if default_throttle < 0:
+            raise ValueError("default_throttle must be a positive number")
         if connection is None:
             connection = redis.StrictRedis()
         self._connection = connection
-        self._name = name
         self._default_throttle = default_throttle
-        self._key = self.redis_round_format.format(name=self._name)
-        if items:
-            self.update(items)
+        self._items_key = self.redis_items_format.format(name=name)
+        self._throttles_key = self.redis_throttles_format.format(name=name)
 
-    def add(self, item, throttle=0):
-        self._connection.zadd(self._key, time() + throttle, item)
+    def update_one(self, item, throttle=None):
+        if throttle is not None:
+            # add or update item throttle
+            added = self._connection.hset(self._throttles_key, item, throttle)
+            # only insert in the queue, don't update the deadline
+            if added:
+                self._connection.zadd(self._items_key, time() + throttle, item)
+        elif not self._connection.hexists(self._throttles_key, item):
+            # new item: add throttle (or default) and push to the queue
+            throttle = self._default_throttle
+            self._connection.hset(self._throttles_key, item, throttle)
+            self._connection.zadd(self._items_key, time() + throttle, item)
+        # else item exists and throttle is None; no-op
 
-    def update(self, items):
-        now = time()
-        if isinstance(items, Mapping):
-            items = {item: now + throttle for item, throttle in items.iteritems()}
-        else:
-            items = dict.fromkeys(items, now + self._default_throttle)
-        self._connection.zadd(self._key, **items)
+    def update_many(self, throttled_items):
+        if not isinstance(throttled_items, Mapping):
+            throttled_items = dict.fromkeys(throttled_items)
+        # split items into new and existing
+        throttled_to_add, throttled_to_update = {}, {}
+        current_items = set(self._connection.hkeys(self._throttles_key))
+        for item, throttle in throttled_items.iteritems():
+            if item not in current_items:
+                throttled_to_add[item] = throttle if throttle is not None else self._default_throttle
+            elif throttle is not None:  # don't update unless explicit throttle is passed
+                throttled_to_update[item] = throttle
+        # push the new items to the queue
+        if throttled_to_add:
+            now = time()
+            self._connection.zadd(self._items_key,
+                                  **{item: now + throttle
+                                   for item, throttle in throttled_to_add.iteritems()})
+        # update the throttles of both existing and new items
+        throttled_to_update.update(throttled_to_add)
+        if throttled_to_update:
+            self._connection.hmset(self._throttles_key, throttled_to_update)
 
     def remove(self, *items):
-        self._connection.zrem(self._key, *items)
+        self._connection.hdel(self._throttles_key, *items)
+        self._connection.zrem(self._items_key, *items)
 
     def clear(self):
-        self._connection.delete(self._key)
+        self._connection.delete(self._throttles_key, self._items_key)
 
     def next(self):
         raise NotImplementedError
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from itertools import islice
 import unittest
 
 import redis
@@ -27,51 +28,82 @@ class RedisTestCase(unittest.TestCase):
 
 class RedRobinTestCase(RedisTestCase):
 
-    def RoundRobin(self, items=None, default_throttle=0):
-        return redrobin.RoundRobin(items=items, default_throttle=default_throttle,
-                                   name='test', connection=self.test_conn)
+    def RoundRobin(self, throttled_items=None, default_throttle=0):
+        rr = redrobin.RoundRobin(default_throttle=default_throttle, name='test',
+                                 connection=self.test_conn)
+        if throttled_items:
+            rr.update_many(throttled_items)
+        return rr
 
-    def get_queue(self, round_robin, withscores=False):
-        return self.test_conn.zrange(round_robin._key, 0, -1, withscores=withscores)
+    def assertQueuesThrottles(self, round_robin, expected_queues, expected_throttled_items):
+        queue = self.test_conn.zrange(round_robin._items_key, 0, -1)
+        throttled_items = self.test_conn.hgetall(round_robin._throttles_key)
+        for item, throttle in throttled_items.iteritems():
+            throttled_items[item] = float(throttle)
+        self.assertEqual(set(queue), set(throttled_items))
+        self.assertEqual(queue, expected_queues)
+        self.assertEqual(throttled_items, expected_throttled_items)
 
-    def test_init_empty(self):
-        self.assertEqual(self.get_queue(self.RoundRobin()), [])
-
-    def test_init_from_mapping(self):
-        rr = self.RoundRobin({'foo': 3, 'bar': 4, 'baz': 2})
-        self.assertEqual(self.get_queue(rr), ['baz', 'foo', 'bar'])
-
-    def test_init_from_sequence(self):
-        items = ['foo', 'bar', 'baz']
-        for rr in self.RoundRobin(items), self.RoundRobin(items, default_throttle=2):
-            q, scores = zip(*self.get_queue(rr, withscores=True))
-            self.assertEqual(len(set(scores)), 1)
-            self.assertEqual(q, ('bar', 'baz', 'foo'))
-
-    def test_add(self):
+    def test_init(self):
         rr = self.RoundRobin()
-        rr.add('foo', 5)
-        rr.add('bar', 4)
-        rr.add('foo', 3)
-        rr.add('baz', 2)
-        self.assertEqual(self.get_queue(rr), ['baz', 'foo', 'bar'])
+        self.assertQueuesThrottles(rr, [], {})
+        self.assertRaises(ValueError, self.RoundRobin, default_throttle=None)
+
+    def test_update_one(self):
+        rr = self.RoundRobin()
+        rr.update_one('foo', 5)
+        self.assertQueuesThrottles(rr, ['foo'], {'foo': 5})
+
+        rr.update_one('bar', 4)
+        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 5, 'bar': 4})
+
+        # neither queue nor throttle of foo is updated
+        rr.update_one('foo')
+        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 5, 'bar': 4})
+
+        # queue not updated but throttle of foo is
+        rr.update_one('foo', 3)
+        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 3, 'bar': 4})
+
+        # new item with default throttle(=0) added
+        rr.update_one('xyz')
+        self.assertQueuesThrottles(rr, ['xyz', 'bar', 'foo'], {'foo': 3, 'bar': 4, 'xyz': 0})
+
+    def test_update_many(self):
+        rr = self.RoundRobin()
+        rr.update_many(dict.fromkeys(['foo', 'bar'], 5))
+        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 5, 'bar': 5})
+
+        # baz and xyz are pushed, foo updates its throttle but stays in the same position
+        rr.update_many(dict.fromkeys(['baz', 'foo', 'xyz'], 2))
+        self.assertQueuesThrottles(rr, ['baz', 'xyz', 'bar', 'foo'],
+                                   {'foo': 2, 'bar': 5, 'baz': 2, 'xyz': 2})
+
+        rr.update_many({'uvw': 3, 'foo': 4})
+        self.assertQueuesThrottles(rr, ['baz', 'xyz', 'uvw', 'bar', 'foo'],
+                                   {'foo': 4, 'bar': 5, 'baz': 2, 'xyz': 2, 'uvw': 3})
+
+        # default throttle(=0) used only when adding new items, not updating existing ones
+        rr.update_many(['uvw', 'qa', 'bar'])
+        self.assertQueuesThrottles(rr, ['qa', 'baz', 'xyz', 'uvw', 'bar', 'foo'],
+                                   {'foo': 4, 'bar': 5, 'baz': 2, 'xyz': 2, 'uvw': 3, 'qa': 0})
 
     def test_remove_existing(self):
         rr = self.RoundRobin({'foo': 3, 'bar': 4, 'baz': 2})
         rr.remove('foo')
-        self.assertEqual(self.get_queue(rr), ['baz', 'bar'])
+        self.assertQueuesThrottles(rr, ['baz', 'bar'], {'bar': 4, 'baz': 2})
 
     def test_remove_missing(self):
         rr = self.RoundRobin({'foo': 3, 'bar': 4, 'baz': 2})
         rr.remove('xyz')
-        self.assertEqual(self.get_queue(rr), ['baz', 'foo', 'bar'])
+        self.assertQueuesThrottles(rr, ['baz', 'foo', 'bar'], {'foo':3, 'bar': 4, 'baz': 2})
 
     def test_remove_multiple(self):
         rr = self.RoundRobin({'foo': 3, 'bar': 4, 'baz': 2})
         rr.remove('baz', 'xyz', 'bar')
-        self.assertEqual(self.get_queue(rr), ['foo'])
+        self.assertQueuesThrottles(rr, ['foo'], {'foo': 3})
 
     def test_clear(self):
         rr = self.RoundRobin({'foo': 3, 'bar': 4, 'baz': 2})
         rr.clear()
-        self.assertEqual(self.get_queue(rr), [])
+        self.assertQueuesThrottles(rr, [], {})
