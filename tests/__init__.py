5091a6829d1735461ee8f3d8b940492761239bf6
George Sakkis <george.sakkis@gmail.com>
2014-11-10 17:38:52 +0200

    Move RedisTestCase and MockTime to tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,53 @@
+import unittest
+
+import mock
+import redis
+
+
+MOCK_TIME = True
+TIME_DELTA = 1e-3
+
+
+class MockTime(object):
+
+    @classmethod
+    def patch(cls, now=0, tick=TIME_DELTA):
+        if not MOCK_TIME:
+            return lambda f: f
+        self = cls(now, tick)
+        return mock.patch.multiple('time', time=self.time, sleep=self.sleep)
+
+    def __init__(self, now, tick):
+        self.now = now
+        self.tick = tick
+
+    def time(self):
+        # add some minimum (virtual) delay between calls to time()
+        self.sleep(self.tick)
+        return self.now
+
+    def sleep(self, n):
+        self.now += n
+
+
+class RedisTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for dbnum in range(16):
+            test_conn = redis.StrictRedis(db=dbnum)
+            cursor, results = test_conn.scan(0)
+            if cursor == 0 and not results:  # empty
+                cls.test_conn = test_conn
+                return
+        assert False, 'No empty Redis database found to run tests in'
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_conn.flushdb()
+
+    def setUp(self):
+        self.test_conn.flushdb()
+
+    def tearDown(self):
+        self.test_conn.flushdb()
