31f88dce6ccb1da2de5a8ed5d94f7fc836c5cb86
George Sakkis <george.sakkis@gmail.com>
2014-11-16 10:29:10 +0200

    s/assertQueuesThrottles/assertQueueThrottles/g

--- tests/test_multithrottling.py
+++ tests/test_multithrottling.py
@@ -11,51 +11,51 @@ class MultiThrottleBalancerTestCase(BaseTestCase):
         return redrobin.MultiThrottleBalancer(throttled_keys=throttled_keys,
                                               name=name, connection=self.test_conn)
 
-    def assertQueuesThrottles(self, round_robin, expected_queues, expected_throttled_keys):
+    def assertQueueThrottles(self, round_robin, expected_queue, expected_throttled_keys):
         queue = self.test_conn.zrange(round_robin.queue_key, 0, -1)
+        self.assertEqual(queue, expected_queue)
         throttled_keys = dict(round_robin.iteritems())
         self.assertItemsEqual(queue, throttled_keys.keys())
-        self.assertEqual(queue, expected_queues)
         self.assertEqual(throttled_keys, expected_throttled_keys)
 
     def test_init(self):
         rr = self.get_balancer()
-        self.assertQueuesThrottles(rr, [], {})
+        self.assertQueueThrottles(rr, [], {})
 
         rr = self.get_balancer({'foo': 3, 'bar': 4})
-        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 3, 'bar': 4})
+        self.assertQueueThrottles(rr, ['bar', 'foo'], {'foo': 3, 'bar': 4})
 
         rr = self.get_balancer({'abc': 1, 'xyz': 2, 'foo': 4})
-        self.assertQueuesThrottles(rr, ['abc', 'foo', 'xyz'],
+        self.assertQueueThrottles(rr, ['abc', 'foo', 'xyz'],
                                    {'abc': 1, 'xyz': 2, 'foo': 4})
 
     def test_fromkeys(self):
         rr = redrobin.MultiThrottleBalancer.fromkeys([], name='test',
                                                     connection=self.test_conn)
-        self.assertQueuesThrottles(rr, [], {})
+        self.assertQueueThrottles(rr, [], {})
 
         rr = redrobin.MultiThrottleBalancer.fromkeys(['foo', 'bar'], 1,
                                                       name='test',
                                                       connection=self.test_conn)
-        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 1, 'bar': 1})
+        self.assertQueueThrottles(rr, ['bar', 'foo'], {'foo': 1, 'bar': 1})
 
         rr = redrobin.MultiThrottleBalancer.fromkeys(['abc', 'xyz', 'foo'], 0,
                                                       name='test',
                                                       connection=self.test_conn)
-        self.assertQueuesThrottles(rr, ['abc', 'foo', 'xyz'],
+        self.assertQueueThrottles(rr, ['abc', 'foo', 'xyz'],
                                    {'abc': 0, 'xyz': 0, 'foo': 0})
 
     def test_setitem(self):
         rr = self.get_balancer()
         rr['foo'] = 5
-        self.assertQueuesThrottles(rr, ['foo'], {'foo': 5})
+        self.assertQueueThrottles(rr, ['foo'], {'foo': 5})
 
         rr['bar'] = 4
-        self.assertQueuesThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
+        self.assertQueueThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
 
         # queue not updated but throttle of foo is
         rr['foo'] = 3
-        self.assertQueuesThrottles(rr, ['foo', 'bar'], {'foo': 3, 'bar': 4})
+        self.assertQueueThrottles(rr, ['foo', 'bar'], {'foo': 3, 'bar': 4})
 
         # invalid throttle
         for throttle in -1, '1', None:
@@ -65,13 +65,13 @@ class MultiThrottleBalancerTestCase(BaseTestCase):
     def test_setdefault(self):
         rr = self.get_balancer()
         rr.setdefault('foo', 5)
-        self.assertQueuesThrottles(rr, ['foo'], {'foo': 5})
+        self.assertQueueThrottles(rr, ['foo'], {'foo': 5})
 
         rr.setdefault('bar', 4)
-        self.assertQueuesThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
+        self.assertQueueThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
 
         rr.setdefault('foo', 3)
-        self.assertQueuesThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
+        self.assertQueueThrottles(rr, ['foo', 'bar'], {'foo': 5, 'bar': 4})
 
         # invalid throttle
         self.assertRaises(ValueError, rr.setdefault, 'foo')
@@ -82,56 +82,56 @@ class MultiThrottleBalancerTestCase(BaseTestCase):
     def test_delitem(self):
         rr = self.get_balancer({'foo': 3, 'bar': 4, 'baz': 2})
         del rr['foo']
-        self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+        self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
         with self.assertRaises(KeyError):
             del rr['xyz']
-            self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+            self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
     def test_pop(self):
         rr = self.get_balancer({'foo': 3, 'bar': 4, 'baz': 2})
         self.assertEqual(rr.pop('foo'), 3)
-        self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+        self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
         with self.assertRaises(KeyError):
             rr.pop('xyz')
-            self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+            self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
         self.assertEqual(rr.pop('xyz', -1), -1)
-        self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+        self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
     def test_popitem(self):
         rr = self.get_balancer({'foo': 3, 'bar': 4, 'baz': 2})
 
         self.assertEqual(rr.popitem(), ('baz', 2))
-        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'bar': 4, 'foo': 3})
+        self.assertQueueThrottles(rr, ['bar', 'foo'], {'bar': 4, 'foo': 3})
 
         self.assertEqual(rr.popitem(), ('foo', 3))
-        self.assertQueuesThrottles(rr, ['bar'], {'bar': 4})
+        self.assertQueueThrottles(rr, ['bar'], {'bar': 4})
 
         self.assertEqual(rr.popitem(), ('bar', 4))
-        self.assertQueuesThrottles(rr, [], {})
+        self.assertQueueThrottles(rr, [], {})
 
         self.assertRaises(KeyError, rr.popitem)
 
     def test_update(self):
         rr = self.get_balancer()
         rr.update(dict.fromkeys(['foo', 'bar'], 5))
-        self.assertQueuesThrottles(rr, ['bar', 'foo'], {'foo': 5, 'bar': 5})
+        self.assertQueueThrottles(rr, ['bar', 'foo'], {'foo': 5, 'bar': 5})
 
         # baz and xyz are pushed, foo updates its throttle but stays in the same position
         rr.update(dict.fromkeys(['baz', 'foo', 'xyz'], 2))
-        self.assertQueuesThrottles(rr, ['bar', 'foo', 'baz', 'xyz'],
+        self.assertQueueThrottles(rr, ['bar', 'foo', 'baz', 'xyz'],
                                    {'foo': 2, 'bar': 5, 'baz': 2, 'xyz': 2})
 
         # update from kwargs
         rr.update(xyz=3, abc=4)
-        self.assertQueuesThrottles(rr, ['bar', 'foo', 'baz', 'xyz', 'abc'],
+        self.assertQueueThrottles(rr, ['bar', 'foo', 'baz', 'xyz', 'abc'],
                                    {'foo': 2, 'bar': 5, 'baz': 2, 'xyz': 3, 'abc': 4})
 
         # update from both a dict and kwargs
         rr.update({'bar': 2, 'mno': 8}, foo=1, ghi=7)
-        self.assertQueuesThrottles(rr, ['bar', 'foo', 'baz', 'xyz', 'abc', 'ghi', 'mno'],
+        self.assertQueueThrottles(rr, ['bar', 'foo', 'baz', 'xyz', 'abc', 'ghi', 'mno'],
                                    {'foo': 1, 'bar': 2, 'baz': 2, 'xyz': 3,
                                     'abc': 4, 'mno': 8, 'ghi': 7})
 
@@ -142,18 +142,18 @@ class MultiThrottleBalancerTestCase(BaseTestCase):
     def test_discard(self):
         rr = self.get_balancer({'foo': 3, 'bar': 4, 'baz': 2})
         rr.discard('foo')
-        self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+        self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
         rr.discard('xyz')
-        self.assertQueuesThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
+        self.assertQueueThrottles(rr, ['bar', 'baz'], {'bar': 4, 'baz': 2})
 
         rr.discard('baz', 'xyz')
-        self.assertQueuesThrottles(rr, ['bar'], {'bar': 4})
+        self.assertQueueThrottles(rr, ['bar'], {'bar': 4})
 
     def test_clear(self):
         rr = self.get_balancer({'foo': 3, 'bar': 4, 'baz': 2})
         rr.clear()
-        self.assertQueuesThrottles(rr, [], {})
+        self.assertQueueThrottles(rr, [], {})
 
     def test_keys(self):
         rr = self.get_balancer({'x': 3, 'y': 4, 'z': 2}, name='diff_throttles')
